76fd8a9f9294dc99e481019c4558d3fe1540df4e
naresh546nk <91820009+naresh546nk@users.noreply.github.com>
2023-02-15 19:55:28 +0530

    added the version number

--- src/main/java/com/example/awscodepieline/controller/UserController.java
+++ src/main/java/com/example/awscodepieline/controller/UserController.java
@@ -4,6 +4,7 @@ package com.example.awscodepieline.controller;
 import com.example.awscodepieline.model.User;
 import com.example.awscodepieline.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +16,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private Environment environment;
+
 
     @GetMapping("/")
     public String home(){
-        return  "Application is running fine ...";
+        return  "Application is running fine version: 1" ;
     }
 
     @GetMapping("/users")
